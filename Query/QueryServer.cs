69b35586b6ba7c79487c00704ea78d0bfd0c6784
Nicolas Granger <Nicolas62x@gmail.com>
2021-12-01 15:27:22 +0100

    V1.0

--- Query/QueryServer.cs
+++ Query/QueryServer.cs
@@ -4,19 +4,6 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-
-QueryServer query = new QueryServer(
-    //theses anonimous function can be any function with a prototype of BaseServerInfo function() and BaseServerInfo FullServerInfo()
-    () => new QueryServer.BasicServerInfo { MOTD = "A Query Server !", GameType = "Testing", MapName = "None", NumPlayers = "42", MaxPlayers = "69", HostPort = 19132, HostIp = "127.0.0.1" },
-    () => new QueryServer.FullServerInfo { MOTD = "A Query Server !", GameType = "Testing", GameID = "MINECRAFT", Plugins = "", Players = new string[] { "Nicolas61x", "dadodasyra", "Zelytra" }, Version = "1.18.0", MapName = "None", NumPlayers = "42", MaxPlayers = "69", HostPort = 19132, HostIp = "127.0.0.1" },
-    19132
-    );
-
-//run the query server for 60s
-Thread.Sleep(60000);
-
-query.StopListening();
-
 /// <summary>
 /// Class used to listen for minecraft querys and respond with data provided by callbacks
 /// </summary>
