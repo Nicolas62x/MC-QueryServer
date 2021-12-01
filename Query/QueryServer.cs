f300c49df72b3b82a77c809139e689239ea5e969
Nicolas Granger <Nicolas62x@gmail.com>
2021-12-01 10:18:01 +0100

    added async send and token generation and checking

--- Query/QueryServer.cs
+++ Query/QueryServer.cs
@@ -9,22 +9,19 @@ query.StartListening(19132);
 
 query.OnReceiveCB = (EndPoint ip, byte[] buffer, int len) =>
 {
-    Console.WriteLine($"Received from {ip}: {BitConverter.ToString(buffer, 0, len)}");
+    Console.WriteLine($"Received from {ip} ({len}) {BitConverter.ToString(buffer, 0, len)}");
     return true;
 };
 
 Socket s = new Socket(SocketType.Dgram, ProtocolType.Udp);
 
-s.Bind(new IPEndPoint(IPAddress.Loopback, 0));
+s.Bind(new IPEndPoint(IPAddress.IPv6Any, 0));
 
-Socket s2 = new Socket(SocketType.Dgram, ProtocolType.Udp);
+s.SendTo(new byte[] { 0xFE, 0xFD, 0x9, 0x42, 0x73, 0x24, 0x69 }, new IPEndPoint(IPAddress.IPv6Loopback, 19132));
+Thread.Sleep(200);
+s.SendTo(new byte[] { 0xFE, 0xFD, 0x0, 0x42, 0x73, 0x24, 0x69 , 0, 0, 0, 0}, new IPEndPoint(IPAddress.IPv6Loopback, 19132));
 
-s2.Bind(new IPEndPoint(IPAddress.IPv6Loopback, 0));
-
-s.SendTo(new byte[] { 0x42, 0x73, 0x89, 0x42 }, new IPEndPoint(IPAddress.Loopback, 19132));
-s2.SendTo(new byte[] { 0x42, 0x73, 0x89, 0x42, 0x02 }, new IPEndPoint(IPAddress.IPv6Loopback, 19132));
-
-Thread.Sleep(1000);
+Thread.Sleep(5000);
 
 query.StopListening();
 
@@ -33,10 +30,10 @@ query.StopListening();
 /// </summary>
 class QueryServer
 {
-    const int MaxListenRetry = 10;
+    const int MaxSocketRetry = 10;
 
     //Query Protocol
-    const ushort Magic = 0xFDFE;
+    const ushort Magic = 0xFEFD;
 
     static ArrayPool<byte> Bytes = ArrayPool<byte>.Shared;
     
@@ -95,9 +92,17 @@ class QueryServer
             if (s is null)
                 throw new InvalidOperationException("Server is not started");
 
-            s.Dispose();
+            Socket tmp = s;
+            s = null;
+            tmp.Dispose();
         }        
     }
+
+    Random r = new Random();
+    PriorityQueue<int, DateTime> TokenExpiration = new PriorityQueue<int, DateTime>();
+    Dictionary<int, EndPoint> TokenToEndPoint = new Dictionary<int, EndPoint>();
+
+    object TokenLock = new object();
 
     void HandlePacket(byte[] buffer, int len, EndPoint address)
     {
@@ -107,37 +112,179 @@ class QueryServer
             if (!cb(address, buffer, len))
                 return;
 
+        int ptr = 0;
+
+        if (buffer[ptr++] != 0xFE || buffer[ptr++] != 0xFD)
+            return;
+
+        byte type = buffer[ptr++];
+
+        int session = BitConverter.ToInt32(buffer, ptr);
+        ptr += sizeof(int);
+
+        if (TokenExpiration.Count > 0)
+            lock (TokenLock)
+            {
+                while (TokenExpiration.TryPeek(out int token, out DateTime expiration))
+                {
+                    if (expiration < DateTime.Now)
+                    {
+                        int tok = TokenExpiration.Dequeue();
+                        TokenToEndPoint.Remove(tok);
+                    }
+                    else
+                        break;
+                }
+            }
+        
+
+        switch (type)
+        {
+            case 9:
+
+                if (len == 7)
+                {
+                    //generating response and challenge token
+                    int token;
+
+                    lock (TokenLock)
+                    {
+                        do
+                        {
+                            token = r.Next();
+                        }
+                        while (TokenToEndPoint.ContainsKey(token));
+
+                        TokenToEndPoint.Add(token, address);
+                        TokenExpiration.Enqueue(token, DateTime.Now.AddSeconds(30));
+                    }
+
+                    List<byte> response = new List<byte>();
+                    
+                    response.Add(9);
+                    response.AddRange(BitConverter.GetBytes(session));
+
+                    ReadOnlySpan<char> tok = token.ToString().AsSpan();
+
+                    for (int i = 0; i < tok.Length; i++)
+                    {
+                        response.Add((byte)tok[i]);
+                    }
+
+                    response.Add(0);
+
+                    Send(new SendPacket { data = response.ToArray(), ep = address });
+                }
+
+                break;
+
+            case 0:
+
+                if (len == 11)
+                {
+                    if (!CheckToken(buffer, ref ptr, address))
+                        return;
+                }
+                else if (len == 15)
+                {
+                    if (!CheckToken(buffer, ref ptr, address))
+                        return;
+                }
+
+                break;
+        }
+    }
+
+    bool CheckToken(byte[] buffer, ref int ptr, EndPoint address)
+    {
+        int token = buffer[ptr++] << 24 | buffer[ptr++] << 16 | buffer[ptr++] << 8 | buffer[ptr++];
+
+        lock (TokenLock)
+            if (!TokenToEndPoint.TryGetValue(token, out EndPoint? ep) || !ep.Equals(address))
+                return false;
+        return true;
+    }
+
+    struct SendPacket
+    {
+        public byte[] data;
+        public EndPoint ep;
+    }
+
+    object SendLock = new object();
+    Queue<SendPacket> packets = new Queue<SendPacket>();
+    bool Sending = false;
+
+    void Send(SendPacket packet)
+    {
+
+        if (s is null)
+            throw new NullReferenceException("Socket was null");
+
+        lock (SendLock)
+        {
+            if (Sending)
+            {
+                packets.Enqueue(packet);
+            }
+            else
+            {
+                int c = 0;
+                retry:
+
+                try
+                {
+                    s.BeginSendTo(packet.data, 0, packet.data.Length, SocketFlags.None, packet.ep, OnSend, this);
+                    Sending = true;
+                }
+                catch (Exception)
+                {
+                    if (c++ < MaxSocketRetry)
+                        goto retry;
+                }                
+            }
+        }
 
     }
 
-    static void OnReceive(IAsyncResult res)
+    static void OnSend(IAsyncResult res)
     {
         QueryServer? server = (QueryServer?)res.AsyncState;
 
-        if (server is null || server.s is null || server.buffer is null)
+        if (server is null || server.s is null)
             return;
-
-        EndPoint ip = new IPEndPoint(IPAddress.IPv6Any, 0);
-
-        int c = server.s.EndReceiveFrom(res, ref ip);
-
-        byte[] buffer = server.buffer;
-
-        server.Listen();
 
         try
         {
-            if (c > 0)
-                server.HandlePacket(buffer, c, ip);
+            server.s.EndSendTo(res);
         }
         catch (Exception)
         {
         }
-        finally
+        
+        lock (server.SendLock)
         {
-            Bytes.Return(buffer);
-        }
+            if (server.packets.Count > 0)
+            {
 
+                SendPacket packet = server.packets.Dequeue();
+
+                int c = 0;
+                retry:
+
+                try
+                {
+                    server.s.BeginSendTo(packet.data, 0, packet.data.Length, SocketFlags.None, packet.ep, OnSend, server);
+                }
+                catch (Exception)
+                {
+                    if (c++ < MaxSocketRetry)
+                        goto retry;
+                }
+            }
+            else
+                server.Sending = false;
+        }
     }
 
     void Listen()
@@ -157,8 +304,46 @@ class QueryServer
         }
         catch (Exception)
         {
-            if (c++ < MaxListenRetry)
+            if (c++ < MaxSocketRetry)
                 goto retry;
         }
+    }
+
+    static void OnReceive(IAsyncResult res)
+    {
+        QueryServer? server = (QueryServer?)res.AsyncState;
+
+        if (server is null || server.s is null || server.buffer is null)
+            return;
+
+        EndPoint ip = new IPEndPoint(IPAddress.IPv6Any, 0);
+
+        int c = 0;
+
+        try
+        {
+            c = server.s.EndReceiveFrom(res, ref ip);
+        }
+        catch (Exception)
+        {
+        }
+
+        byte[] buffer = server.buffer;
+
+        server.Listen();
+
+        try
+        {
+            if (c > 6)
+                server.HandlePacket(buffer, c, ip);
+        }
+        catch (Exception)
+        {
+        }
+        finally
+        {
+            Bytes.Return(buffer);
+        }
+
     }
 }
